830a9642cb80432fdf57eec9a821b68316d6b4c7
Gautham <appskans2017@gmail.com>
2022-03-09 02:46:49 +0530

    gi

--- hamburger.js
+++ hamburger.js
@@ -1,12 +1,15 @@
 let hamMenu = document.getElementById("hmenu");
 let normalMenu = document.getElementById("nmenu");
+let quotesel = document.getElementById("quoteid");
 
 hamMenu.onclick = function () {
     if (normalMenu.className == 'normalmenu') {
         normalMenu.className = 'hamburgermenu';
+        quotesel.className = 'quote quotepostioned';
     }
 
     else if(normalMenu.className=='hamburgermenu'){
         normalMenu.className = 'normalmenu';
+        quotesel.className = 'quote';
     }
 }    
